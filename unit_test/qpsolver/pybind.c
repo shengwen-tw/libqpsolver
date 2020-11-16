c6c76428a0665f3d212e42717f949b3c9ae38933
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-11-16 23:33:08 +0800

    call libqpsolver in python3 (only print out the data for now)

--- unit_test/qpsolver/pybind.c
+++ unit_test/qpsolver/pybind.c
@@ -1,16 +1,55 @@
+#include <iostream>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
 #include "libqpsolver.h"
 #include "qpsolver.h"
+
+using namespace std;
+namespace py = pybind11;
+
+using pyarray = std::optional<py::array_t<FLOAT>>;
+
+void print_py_array(string prompt, pyarray &py_arr)
+{
+    /* use C++17 std::optional to identify if python object exists (!= None) */
+    if(py_arr.has_value() == true) {
+        printf("%s (%dx%d) =\n",
+               prompt.c_str(),
+               (int)py_arr.value().shape(0),
+               (int)py_arr.value().shape(1));
+
+        auto data = py_arr.value().unchecked<2>();
+        for (py::ssize_t r = 0; r < data.shape(0); r++) {
+            printf("   ");
+            for (py::ssize_t c = 0; c < data.shape(1); c++) {
+                printf("%f ", data(r, c));
+            }
+            printf("\n");
+        }
+    }
+}
+
+void my_quadprog(pyarray P,    pyarray q,
+                 pyarray A,    pyarray b,
+                 pyarray A_eq, pyarray b_eq,
+                 pyarray lb,   pyarray ub)
+{
+    print_py_array("P", P);
+    print_py_array("q", q);
+    print_py_array("A", A);
+    print_py_array("b", b);
+    print_py_array("A_eq", A_eq);
+    print_py_array("b_eq", b_eq);
+    print_py_array("lb", lb);
+    print_py_array("ub", ub);
+
+    qp_t qp;
+    qp_set_default(&qp);
+}
 
 PYBIND11_MODULE(libqpsolver, qp)
 {
 	qp.doc() = "libqpsolver, a quadratic programming library written in C";
-	qp.def("solver_set_default", &qp_set_default, "");
-	qp.def("set_optimization_variable", &qp_solve_set_optimization_variable, "");
-	qp.def("set_cost_function", &qp_solve_set_cost_function, "");
-	qp.def("set_equality_constraint", &qp_solve_set_equality_constraints, "");
-	qp.def("set_upper_bound", &qp_solve_set_upper_bound_inequality_constraints, "");
-	qp.def("set_lower_bound", &qp_solve_set_lower_bound_inequality_constraints, "");
-	qp.def("set_inequality_constraint", &qp_solve_set_affine_inequality_constraints, "");
-	qp.def("solve", &qp_solve_start, "");
+	qp.def("my_quadprog", &my_quadprog, "");
 }
