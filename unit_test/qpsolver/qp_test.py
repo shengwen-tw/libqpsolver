36f01012e5e9ed74bf1d378cf05b58770ebe8c70
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-11-21 23:26:07 +0800

    unit_test: test objective functions with multiple scales

--- unit_test/qpsolver/qp_test.py
+++ unit_test/qpsolver/qp_test.py
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 '''
 do not directly run this script, you should execute the unit test
 by launching the "run_test.sh"
@@ -38,7 +36,8 @@ def quadprog_cvxopt(P, q, A=None, b=None, A_eq=None, b_eq=None, options=None):
         A_eq, b_eq = None, None
 
     #solve qp
-    return solvers.qp(P, q, A, b, A_eq, b_eq, options);
+    sol = solvers.qp(P, q, A, b, A_eq, b_eq, options);
+    return np.array(sol['x']);
 
 def matrix_compare(mat1, mat2):
     epsilon = 1e-3;
@@ -54,11 +53,26 @@ def matrix_compare(mat1, mat2):
 
     return True
 
-def test_qp():
-    P = np.array([[+3.0, -1.0],
-                  [-1.0, +4.0]])
-    q = np.array([[-2.0],
-                  [-6.0]])
+def generate_symmetric_positive_definite_matrix(row, column, max_val):
+    vec = np.random.rand(row, column)
+    vec_transposed = np.transpose(vec)
+
+    spd_matrix = np.matmul(vec, vec_transposed)
+    spd_matrix = np.multiply(max_val, spd_matrix)
+
+    return spd_matrix
+
+def generate_random_row_vector(row, max_val):
+    vec = np.random.rand(row, 1)
+    vec = np.multiply(max_val, vec)
+
+    return vec
+
+def test_random_2x2_qp_problem(cost_func_max_val):
+    P = generate_symmetric_positive_definite_matrix(2, 2, cost_func_max_val);
+
+    q = generate_random_row_vector(2, cost_func_max_val)
+
     A = np.array([[+1.0, +1.0],
                   [-1.0, +2.0],
                   [+2.0, +1.0]])
@@ -68,11 +82,7 @@ def test_qp():
     A_eq = np.array([[1.0, 1.0]])
     b_eq = np.array([[0.0]])
 
-    cvxopt_sol = quadprog_cvxopt(P, q, A, b, A_eq, b_eq, None)
-    cvxopt_sol = np.array(cvxopt_sol['x'])
-
-    print('Test matrices:')
-
+    print('Test input matrices:')
     print('P = \n%s' %(P))
     print('q = \n%s' %(q))
     print('A = \n%s' %(A))
@@ -80,15 +90,29 @@ def test_qp():
     print('A_eq = \n%s' %(A_eq))
     print('b_eq = \n%s' %(b_eq))
 
-    print('\nOptimal solution given by cvxopy:')
+    cvxopt_sol = quadprog_cvxopt(P, q, A, b, A_eq, b_eq, None)
+    libqpsolver_sol = libqpsolver.quadprog(P, q, A, b, A_eq, b_eq, None, None)
+
+    print('\nOptimal solution given cvxopt:')
     print(cvxopt_sol)
 
-    libqpsolver_sol = libqpsolver.quadprog(P, q, A, b, A_eq, b_eq, None, None)
+    print('\nOptimal solution by libqpsolver')
     print(libqpsolver_sol)
 
-    if matrix_compare(cvxopt_sol, libqpsolver_sol) == True:
+    test_result = matrix_compare(cvxopt_sol, libqpsolver_sol)
+
+    if test_result == True:
         print('\nunit test passed')
     else:
-        print('\nerror, unit test did not passed');
+        print('\nerror, unit test did not passed')
 
-test_qp()
+    print('=============================================================')
+
+    return test_result
+
+def main():
+    test_random_2x2_qp_problem(100)
+    test_random_2x2_qp_problem(500)
+    test_random_2x2_qp_problem(1000)
+
+if __name__ == "__main__": main()
