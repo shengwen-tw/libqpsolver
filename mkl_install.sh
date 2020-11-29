41ce4de8dfc3cf8b74ab3014e9148ecdf49b04cf
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-11-29 20:31:50 +0800

    update intel mkl installation script

--- mkl_install.sh
+++ mkl_install.sh
@@ -4,7 +4,10 @@
 wget https://registrationcenter-download.intel.com/akdlm/irc_nas/tec/16903/l_mkl_2020.3.279.tgz
 tar -zxvf l_mkl_2020.3.279.tgz
 cd l_mkl_2020.3.279
-sudo ./install_GUI.sh
+export MKL_INSTALL_DIR=/opt/intel
+echo "ACCEPT_EULA=accept" >> silent.cfg
+echo "PSET_INSTALL_DIR=${MKL_INSTALL_DIR}" >> silent.cfg
+sudo ./install.sh --user-mode -s ./silent.cfg
 
 #append LD_LBRARY_PATH export command in .bashrc
 INSTALL_PATH=/opt/intel/mkl
