150429d044d1255ac4ed95e59540d844d5841d11
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-12-05 22:14:41 +0800

    unit test the function of solve linear system, matrix inversion, qr factorization, etc.

--- unit_test/matrix/matrix_test.py
+++ unit_test/matrix/matrix_test.py
@@ -8,6 +8,10 @@ import os
 import numpy as np
 
 def test_matrix_functions():
+    ########################
+    # solver linear system #
+    ########################
+
     print('solve AX=B:')
 
     A = np.array([[3.0, 5.0, 2.0],
@@ -22,6 +26,71 @@ def test_matrix_functions():
     print('A = \n%s' %(A))
     print('B = \n%s' %(B))
     print('X = \n%s' %(X))
+
+    ####################
+    # matrix inversion #
+    ####################
+    print('\nsolve matrix inversion inv(A):')
+
+    A = np.array([[ 1.0, 0.0,  2.0],
+                  [-1.0, 5.0,  0.0],
+                  [ 0.0, 3.0, -9.0]])
+
+    A_inv = matrix_wrapper.matrix_inverse(A)
+
+    print('A = \n%s' %(A))
+    print('A_inv = \n%s' %(A_inv))
+
+    #########################
+    # matrix multiplication #
+    #########################
+
+    print('\nsolve matrix multiplication B = AX:')
+
+    A = np.array([[3, 5, 2],
+                  [2, 1, 3],
+                  [4, 3, 2]])
+    X = np.array([[2.0,   38.39],
+                  [9.00, -11.30],
+                  [3.00, -17.82]])
+    B = matrix_wrapper.matrix_multiply(A, B)
+
+    print('A = \n%s' %(A))
+    print('X = \n%s' %(X))
+    print('B = \n%s' %(B))
+
+    ####################
+    # QR factorization #
+    ####################
+
+    print("\nsolve QR factorization:")
+
+    A = np.array([[+1, +4, +0, +1, -3, +2],
+                  [+2, +8, +1, +1, -4, +6],
+                  [-1, -4, -1, +0, +1, -2],
+                  [+1, +4, +0, +1, -3, +1]])
+
+    Q = matrix_wrapper.qr_factorization_Q(A)
+    R = matrix_wrapper.qr_factorization_R(A)
+
+    print('A = \n%s' %(A))
+    print('Q = \n%s' %(Q))
+    print('R = \n%s' %(R))
+
+    ###########################
+    # solve matrix null space #
+    ###########################
+
+    print('\nRank of matrix A:');
+
+    A = np.array([[10, 0,  0,     0],
+                  [0, 25,  0,     0],
+                  [0,  0, 34,     0],
+                  [0,  0,  0, 1e-15]])
+    rank = matrix_wrapper.matrix_rank(A)
+
+    print('A = \n%s' %(A))
+    print('The rank of matrix A is %d' %(rank));
 
 def main():
     test_matrix_functions()
